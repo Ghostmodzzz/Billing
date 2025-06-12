b4408b3f7884072637276c4328053db931b6e1d7
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-12 19:25:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-dev-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY') or ''
